6cfb8c62ade2f2a9f4cdd65736997afe657cb51d
morganhyland <m@morganhyland.com>
2017-01-24 23:56:24 -0500

    check install instagram mobile link

--- js/script.js
+++ js/script.js
@@ -10,9 +10,18 @@ $(document).ready ( function() {
     $(".__form__item").focus();
     // $(".__form__item").select();
     if( /Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent) ) {
-      $(".__link__instagram").attr("href","instagram://user?username=allthearchitects");
+      // $(".__link__instagram").attr("href","instagram://user?username=allthearchitects");
       // $(".__link__facebook").attr("href","fb://profile/1364924453549098");
-      $(".__link__facebook").click(function(){
+
+      $(".__link__instagram" ).click(function(){
+        var now = new Date().valueOf();
+        setTimeout(function () {
+          if (new Date().valueOf() - now > 100) return;
+          window.location = "https://facebook.com/allthearchitects";
+          }, 25);
+        window.location = "instagram://user?username=allthearchitects";
+      });
+      $(".__link__facebook" ).click(function(){
         var now = new Date().valueOf();
         setTimeout(function () {
           if (new Date().valueOf() - now > 100) return;
